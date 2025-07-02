19434bc79d9112738da9f1d60179c1386908036f
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-02 13:29:31 +0600

    debug style removed

--- src/pages/ProductDetailsPage/ProductDetails.jsx
+++ src/pages/ProductDetailsPage/ProductDetails.jsx
@@ -95,7 +95,7 @@ const ProductDetails = () => {
       </div>
 
       {/* Product Details with Delivery Options Container */}
-      <div className="max-w-7xl mx-auto px-6 py-6 border-2 border-green-500">
+      <div className="max-w-7xl mx-auto px-6 py-6">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Product Images */}
           <div className="lg:col-span-1">
